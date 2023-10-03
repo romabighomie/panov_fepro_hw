b5bd1a7c6e4b39d98036118c7dcc9cf2b2ad9fd4
Roman <romanpanov20@gmail.com>
2023-10-03 22:18:48 +0300

    HW-24 Numbers and lines

---
+++ index.js
@@ -0,0 +1,10 @@
+//Task 1
+let row1 = prompt('Enter text 1', 'Hello');
+let row2 = prompt('Enter text 2', 'World');
+let row3 = prompt('Enter text 3', '!');
+
+alert(`Result: ${row1} ${row2} ${row3}`);
+
+//Task 2
+let number = prompt('Enter number', '12345');
+alert(number.split('').sort().join(' '));
