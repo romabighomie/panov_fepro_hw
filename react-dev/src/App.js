0d9f78b6d9d648483e6db2828a3df75e16bad1e5
Roman <romanpanov20@gmail.com>
2023-10-28 21:09:05 +0300

    HW-40

--- react-dev/src/App.js
+++ react-dev/src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+
 function App() {
   return (
-    <>
-    </>
+    <div className='page'>
+      
+      <Header />
+      <Main />
+      
+    </div>
   );
 }
 
---
+++ react-dev/src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import './nav.css';
+
+function Nav() {
+	return(
+		<nav className='nav'>
+			Nav
+		</nav>
+	);
+}
+
+export default Nav;
+
